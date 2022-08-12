8f58f817b719d5a5aaec54c37425f136f6c456cb
maxime ngoe <maxime.ngoe@gmail.com>
2022-08-12 14:53:13 +0100

    ajustement index et package pour séparation de langue

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import messages_fr_cm_cs from "./translations/cs_cm.json";
 //import messages_fr from "./translations/fr.json";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'fr', messages: messages_fr_cm_cs }],
+  "translations": [{ key: 'fr_cs', messages: messages_fr_cm_cs }],
 }
 
-export const LanguageFrModule = (cfg) => {
+export const LanguageFrCmrCSModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
