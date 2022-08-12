213bc94bace482cd115ee9840d27683f661c875b
maxime ngoe <maxime.ngoe@gmail.com>
2022-08-12 10:24:54 +0100

    ajustement traduction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import messages_fr from "./translations/fr.json";
+import messages_fr_cm_cs from "./translations/cs_cm.json";
+//import messages_fr from "./translations/fr.json";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'fr', messages: messages_fr }],
+  "translations": [{ key: 'fr', messages: messages_fr_cm_cs }],
 }
 
 export const LanguageFrModule = (cfg) => {
