c65390179ef01b17176678915c8791e01d07e99e
ArkeupIDComores <arkeupidcomores@gmail.com>
2024-08-27 10:39:35 +0300

    arkeu
    INSERT INTO public."tblLanguages"(
    	"LanguageCode", "LanguageName", "SortOrder", "CountryCode")
    	VALUES ('km','français comorien', null, null);

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import messages_fr from "./translations/fr.json";
-import flatten from "flat";
+import messages_km from "./translations/km.json";
+//import messages_fr from "./translations/fr.json";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'fr', messages: flatten(messages_fr) }],
+  "translations": [{ key: 'km', messages: messages_km}],
 }
 
-export const LanguageFrModule = (cfg) => {
+export const LanguagekmModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
