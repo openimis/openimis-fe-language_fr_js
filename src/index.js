20f12ddad465de34a48576d9f508c6e3915311f9
ArkeupIDComores <arkeupidcomores@gmail.com>
2024-08-28 13:23:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import messages_km from "./translations/km.json";
 //import messages_fr from "./translations/fr.json";
+import flatten from "flat";
+
+
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'km', messages: messages_km}],
+  "translations": [{ key: 'km', messages: flatten(messages_km) }],
 }
-
 export const LanguagekmModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
