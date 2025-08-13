40321e282ec0ce19cdf52fb8867b87f365df04fd
delcroip <patrick.delcroix@swisstph.ch>
2025-08-14 00:11:47 +0200

    chore: not crashing

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       external: [
         /^@babel.*/,
         /^@date-io\/.*/,
-        /^@material-ui\/.*/,
+        /^@mui\/.*/,
         /^@openimis.*/,
         'classnames',
         'clsx',
